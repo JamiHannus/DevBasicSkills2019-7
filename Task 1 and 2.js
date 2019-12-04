db9263add12f5690dfcb58249e502428786f539b
Jami Hannus <t9haja00.students.oamk.fi>
2019-12-04 12:46:14 +0200

    Some more work to do with the task 1 hex still progen for not allowed characters dec field allows e as  number?

--- Task 1 and 2.js
+++ Task 1 and 2.js
@@ -51,11 +51,71 @@ function conversionfrombin(){
     var octa = +document.getElementById("mathOcta").value;
     var hex = +document.getElementById("mathHex").value;
 
-    dec = parseInt(bin,2);
+    var check = "2345678";
+    bin = bin.toString();
 
-    octa = dec.toString(8);
-    hex = dec.toString(16);
-    
+    if(bin.indexOf("2") > -1)
+    { 
+        alert("wrong input")
+        document.getElementById("mathBin").value = 1;
+        conversionfrombin();
+        return;
+    }
+    if(bin.indexOf("3") > -1)
+    { 
+        alert("wrong input")
+        document.getElementById("mathBin").value = 1;
+        conversionfrombin();
+        return;
+    }
+    if(bin.indexOf("4") > -1)
+    { 
+        alert("wrong input")
+        document.getElementById("mathBin").value = 1;
+        conversionfrombin();
+        return;
+    }
+    if(bin.indexOf("5") > -1)
+    { 
+        alert("wrong input")
+        document.getElementById("mathBin").value = 1;
+        conversionfrombin();
+        return;
+    }
+    if(bin.indexOf("6") > -1)
+    { 
+        alert("wrong input")
+        document.getElementById("mathBin").value = 1;
+        conversionfrombin();
+        return;
+    }
+    if(bin.indexOf("7") > -1)
+    { 
+        alert("wrong input")
+        document.getElementById("mathBin").value = 1;
+        conversionfrombin();
+        return;
+    }
+    if(bin.indexOf("8") > -1)
+    { 
+        alert("wrong input")
+        document.getElementById("mathBin").value = 1;
+        conversionfrombin();
+        return;
+    }
+    if(bin.indexOf("9") > -1)
+    { 
+        alert("wrong input")
+        document.getElementById("mathBin").value = 1;
+        conversionfrombin();
+        return;
+    }
+    else{
+        dec = parseInt(bin,2);
+
+        octa = dec.toString(8);
+        hex = dec.toString(16);
+        }
     document.getElementById("mathDec").value = dec;
     document.getElementById("mathOcta").value = octa;
     document.getElementById("mathHex").value = hex;
@@ -66,11 +126,30 @@ function conversionfromocta(){
     var octa = +document.getElementById("mathOcta").value;
     var hex = +document.getElementById("mathHex").value;
 
+    octa = octa.toString();
+    if (octa.indexOf(9) > -1)
+    {
+        alert("wrong input")
+        document.getElementById("mathOcta").value = 7;
+        conversionfromocta();
+        return;
+    }
+    if (octa.indexOf(8) > -1)
+    {
+        alert("wrong input")
+        document.getElementById("mathOcta").value = 7;
+        conversionfromocta();
+        return;
+    }
+    else{
+        
     dec = parseInt(octa,8);
 
     bin = dec.toString(2);
     hex = dec.toString(16);
     
+    }
+
     document.getElementById("mathDec").value = dec;
     document.getElementById("mathBin").value = bin;
     document.getElementById("mathHex").value = hex;
@@ -81,11 +160,23 @@ function conversionfromhex(){
     var octa = +document.getElementById("mathOcta").value;
     var hex = +document.getElementById("mathHex").value;
 
+    var check = /^[a-f]+$/;
+
+    hex = hex.toString();
+
+    if((hex.match(check)))
+    {
     dec = parseInt(hex,16);
 
     bin = dec.toString(2);
     octa = dec.toString(8);
-    
+    }
+    else{  
+        alert("wrong input")
+        document.getElementById("mathHex").value = hex;
+        return
+    }
+
     document.getElementById("mathDec").value = dec;
     document.getElementById("mathBin").value = bin;
     document.getElementById("mathOcta").value = octa;
